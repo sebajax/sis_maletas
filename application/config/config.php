7fc454f51c0f043248251d7bad611c2c5419afdd
Sebas <sebastianituartebonfrisco@MBPdeSebastian.home>
2017-01-21 03:56:50 -0300

    Se arregla el routes
    Se arregla el routes para agregar el login como la pagina principal

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'login';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
