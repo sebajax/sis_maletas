ba3eeb959331a4e83a7e416e447c11e24addc199
Sebas <sebastian.ituarte@gmail.com>
2020-04-24 18:51:43 -0400

    Optimizacion Model Ingreso Caja

--- application/models/IngresoCaja_model.php
+++ application/models/IngresoCaja_model.php
@@ -10,30 +10,6 @@ class IngresoCaja_model extends CI_Model {
         parent::__construct();
         $this->load->database();
     }
-
-    public function insert($data) {
-        $id_aerolinea = 99; //Aerolina 99 NO BDO
-        $numero_bdo   = 1; //Aerolina 99 NO BDO
-        $this->db->trans_start();
-        $data_ingreso_caja = array(
-            'numero'       => $numero_bdo,
-            'id_aerolinea' => $id_aerolinea,
-            'monto'        => $data['monto'],
-        );
-        $data_trans = array(
-            'numero_bdo'          => $numero_bdo,
-            'id_aerolinea'        => $id_aerolinea,
-            'nombre_pasajero'     => $data['tipo_ingreso'],
-            'fecha_llegada'       => $data['fecha'],
-            'domicilio_direccion' => $data['comentario'],
-            'grupo_sector'        => 1,
-            'lugar'               => 1,
-            'valor'               => $data['monto'],
-        );
-        $this->db->insert("ingresos_caja", $data_ingreso_caja);
-        $this->db->insert("transacciones_bdo_cerradas", $data_trans);
-        $this->db->trans_complete();
-    }
     
     public function nextBdoNumber($id_aerolinea) {
         $this->db->select_max('numero');
